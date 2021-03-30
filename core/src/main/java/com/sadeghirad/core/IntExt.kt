28fdbb7fdf1ae5c930919c4471fea4f6e4a22e4d
ThisIsSadeghi <alisadeghi.dev@gmail.com>
2021-03-31 00:54:10 +0430

    Add core IntExt and LongExt.

---
+++ core/src/main/java/com/sadeghirad/core/IntExt.kt
@@ -0,0 +1,10 @@
+package com.sadeghirad.core
+
+/**
+ * Created by Ali Sadeghi
+ * on 31,Mar,2021
+ */
+
+fun Int.isNegative() = this < 0
+fun Int.isPositive() = this > 0
+fun Int.isZero() = this == 0
---
+++ core/src/main/java/com/sadeghirad/core/LongExt.kt
@@ -0,0 +1,10 @@
+package com.sadeghirad.core
+
+/**
+ * Created by Ali Sadeghi
+ * on 31,Mar,2021
+ */
+
+fun Long.isNegative() = this < 0L
+fun Long.isPositive() = this > 0L
+fun Long.isZero() = this == 0L
